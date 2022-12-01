ab24807df87a12553b0a98b15a64857fc7110c25
Akshansh Mishra <47514698+akshansh11@users.noreply.github.com>
2022-12-01 13:13:08 +0100

    Understanding the input function

---
+++ Input function.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 12:41:09 2022
+
+@author: aksha
+"""
+
+input("what is your research interest?")
+
+print("Hello"+input("What is your research interest?"))
+
+print(len(input("Where do you live? ")))
