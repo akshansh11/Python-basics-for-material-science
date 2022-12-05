4db0beb73e5d43f4c276a7f61a10dd43b187ab6c
Akshansh Mishra <47514698+akshansh11@users.noreply.github.com>
2022-12-05 04:43:05 +0100

    Type error and Type conversion

---
+++ Type conversion.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  5 03:56:38 2022
+
+@author: aksha
+"""
+
+print(len(346789))
+
+print(type("SHERLOCK"))
+
+print(type(2))
+
+number=1106
+print(type(number))
+
+number=str(1106)
+print(type(number))
