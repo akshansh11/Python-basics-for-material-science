7f097f06ffb9c32e5f0e9ff65ad76e76e09008e9
Akshansh Mishra <47514698+akshansh11@users.noreply.github.com>
2022-12-06 02:53:31 +0100

    Code for mathematical operations

---
+++ Mathematical operations.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  6 02:14:47 2022
+
+@author: aksha
+"""
+
+a=2
+b=4
+print(a+b)
+
+
+a=2
+b=4
+print(b-a)
+
+
+a=211
+b=450
+print(a*b)
+
+
+a=840
+b=5
+print(a/b)
+
+
+a=5
+b=8
+
+print(a**b)
+
+print(5*5/5+5**3)
